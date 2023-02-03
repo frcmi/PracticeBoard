dc16ab240e81ccb48291740a5411cf4607742e9e
mibrandonh <seesaws_inferno_0a@icloud.com>
2023-02-02 16:42:04 -0800

    Initial Code

---
+++ src/main/java/frc/robot/subsystems/PhotonvisionSubsystem.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PhotonvisionSubsystem extends SubsystemBase {
+  /** Creates a new PhotonvisionSubsystem. */
+  
+  private final Field2d m_field = new Field2d();
+  private final PhotonCamera camera = new PhotonCamera("photonvision");
+  public PhotonvisionSubsystem() {
+    
+    SmartDashboard.putData("PhotonVision", m_field);
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public CommandBase exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    var result = camera.getLatestResult();
+    if(result.hasTargets()) {
+      List<PhotonTrackedTarget> targets = result.getTargets();
+    }
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
