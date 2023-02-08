abb22efba3ea7db1cf18f8c98f91f5db8ac14475
Abhay Raj <abhayaraj06@gmail.com>
2023-02-07 16:30:51 -0800

    code cump

--- src/main/java/frc/robot/subsystems/PhotonvisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonvisionSubsystem.java
@@ -8,20 +8,69 @@ import java.util.List;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonTrackedTarget;
-
+import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Constants.FieldConstants;
+import frc.robot.Constants.VisionConstants;
+import java.util.ArrayList;
+import java.util.Optional;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 public class PhotonvisionSubsystem extends SubsystemBase {
   /** Creates a new PhotonvisionSubsystem. */
-  
+  public PhotonCamera photonCamera;
+  public PhotonPoseEstimator photonPoseEstimator;
   private final Field2d m_field = new Field2d();
-  private final PhotonCamera camera = new PhotonCamera("photonvision");
+
   public PhotonvisionSubsystem() {
+    // Set up a test arena of two apriltags at the center of each driver station set
+    final AprilTag tag18 =
+            new AprilTag(
+                    18,
+                    new Pose3d(
+                            new Pose2d(
+                                    FieldConstants.length,
+                                    FieldConstants.width / 2.0,
+                                    Rotation2d.fromDegrees(180))));
+    final AprilTag tag01 =
+            new AprilTag(
+                    01,
+                    new Pose3d(new Pose2d(0.0, FieldConstants.width / 2.0, Rotation2d.fromDegrees(0.0))));
+    ArrayList<AprilTag> atList = new ArrayList<AprilTag>();
+    atList.add(tag18);
+    atList.add(tag01);
+
+    // TODO - once 2023 happens, replace this with just loading the 2023 field arrangement
+    AprilTagFieldLayout atfl =
+            new AprilTagFieldLayout(atList, FieldConstants.length, FieldConstants.width);
+
+    // Forward Camera
+    photonCamera =
+            new PhotonCamera(
+                    VisionConstants
+                            .cameraName); // Change the name of your camera here to whatever it is in the
+    // PhotonVision UI.
+
+    // Create pose estimator
+    photonPoseEstimator =
+            new PhotonPoseEstimator(
+                    atfl, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, photonCamera, VisionConstants.robotToCam);
     
     SmartDashboard.putData("PhotonVision", m_field);
+  }
+
+  public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+    photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+    return photonPoseEstimator.update();
   }
 
   /**
@@ -50,7 +99,7 @@ public class PhotonvisionSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    var result = camera.getLatestResult();
+    var result = photonCamera.getLatestResult();
     if(result.hasTargets()) {
       List<PhotonTrackedTarget> targets = result.getTargets();
     }
